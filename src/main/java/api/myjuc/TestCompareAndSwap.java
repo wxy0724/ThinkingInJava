67b313898dbf13c6761d24c6c817bd74202468e5
wxy0724 <bryan0724@126.com>
2018-08-16 23:55:26 +0800

    CountDownLatch

--- src/main/java/api/myjuc/TestCompareAndSwap.java
+++ src/main/java/api/myjuc/TestCompareAndSwap.java
@@ -7,13 +7,10 @@ public class TestCompareAndSwap {
     public static void main(String[] args) {
         final CompareAndSwap cas = new CompareAndSwap();
         for (int i = 0; i < 10; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    int expectedValue = cas.get();
-                    boolean b = cas.compareAndSet(expectedValue, (int)(Math.random() * 101));
-                    System.out.println(b);
-                }
+            new Thread(() -> {
+                int expectedValue = cas.get();
+                boolean b = cas.compareAndSet(expectedValue, (int)(Math.random() * 101));
+                System.out.println(b);
             }).start();
         }
     }
@@ -23,12 +20,16 @@ public class TestCompareAndSwap {
 class CompareAndSwap {
     private int value;
 
-    //获取内存值
+    /**
+     * 获取内存值
+     */
     public synchronized int get(){
         return value;
     }
 
-    //比较
+    /**
+     * 比较
+     */
     public synchronized int compareAndSwap(int expectedValue, int newValue){
         int oldValue = value;
 
@@ -39,7 +40,9 @@ class CompareAndSwap {
         return oldValue;
     }
 
-    //设置
+    /**
+     * 设置
+     */
     public synchronized boolean compareAndSet(int expectedValue, int newValue){
         return expectedValue == compareAndSwap(expectedValue, newValue);
     }
